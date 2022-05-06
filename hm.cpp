c7b00f3ea7003eb50cf421396123000b276277cb
Gusover01 <gdelangelf2100@alumno.ipn.mx>
2022-05-05 21:12:23 -0500

    Primer ingreso a git

---
+++ hm.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main(){
+
+    array<int, 7> numFibo {};
+
+    cout << "Hola mundo " << numFibo.size() << endl;
+
+    return 0;
+}
